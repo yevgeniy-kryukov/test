360a43dced832b346fe8fd4b5c42fe821e6fd242
YevgeniyKryukov <evgkrukov@gmail.com>
2019-04-14 23:31:56 +0600

    init

---
+++ t1.php
@@ -0,0 +1,26 @@
+<?php
+
+function fourLetterWords($sentence) 
+{
+  $n = 0;
+  $words = explode(' ', $sentence);
+  for ($i = 0; $i < count($words); $i++) {
+    if (strlen($words[$i]) == 4) $n++;
+  }
+  return $n;
+}
+
+var_dump(fourLetterWords('This sentence is fine'));
+
+// multibyte
+function fourLetterWordsMB($sentence) 
+{
+  $n = 0;
+  $words = explode(' ', $sentence);
+  for ($i = 0; $i < count($words); $i++) {
+    if (mb_strlen($words[$i]) == 4) $n++;
+  }
+  return $n;
+}
+
+var_dump(fourLetterWordsMB('Это тест функции'));
---
+++ t2.php
@@ -0,0 +1,30 @@
+<?php
+
+function almostPalindromes($str) 
+{
+	$n = 0;
+	$len = strlen($str);
+	for ($i = 0; $i < $len; $i++) {
+		if ($str[$i] != $str[$len - $i - 1]) {
+			$n++;
+		}
+	}
+	return $n;
+}
+
+var_dump(almostPalindromes('fox'));
+
+// multibyte
+function almostPalindromesMB($str) 
+{
+	$n = 0;
+	$len = mb_strlen($str);
+	for ($i = 0; $i < $len; $i++) {
+		if (mb_substr($str, $i, 1) != mb_substr($str, $len - $i - 1, 1)) {
+			$n++;
+		}
+	}
+	return $n;
+}
+
+var_dump(almostPalindromesMB('топот'));
